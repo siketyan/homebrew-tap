df7ce95783892db237bd642c1789251f594431f0
Naoki Ikeguchi <me@s6n.jp>
2022-06-29 15:15:23 +0900

    feat(jpki-cli): Release v0.2.0

--- Formula/jpki-cli.rb
+++ Formula/jpki-cli.rb
@@ -1,16 +1,16 @@
 class JpkiCli < Formula
   desc "Read certificates, sign and verify documents using your JPKI card"
   homepage "https://github.com/siketyan/jpki-rs"
-  version "0.1.8"
+  version "0.2.0"
   license "LGPL-2.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/siketyan/jpki-rs/releases/download/0.1.8/jpki-cli-x86_64-apple-darwin.tar.gz"
-      sha256 "e84fe854624afe2ec10eb9620c2d0f8a57f9d048d7f4d9d3ad31c14e5e750e26"
+      url "https://github.com/siketyan/jpki-rs/releases/download/0.2.0/jpki-cli-x86_64-apple-darwin.tar.gz"
+      sha256 "53bf9e5414e582443f4c69b91de025226e79039684edbe773f8d74d57df495ae"
     else
-      url "https://github.com/siketyan/jpki-rs/releases/download/0.1.8/jpki-cli-aarch64-apple-darwin.tar.gz"
-      sha256 "bea9c51b3ebade43d76ae1c39a30f7e2c703f97330d1c7432c6ed53a58317377"
+      url "https://github.com/siketyan/jpki-rs/releases/download/0.2.0/jpki-cli-aarch64-apple-darwin.tar.gz"
+      sha256 "f5273059d2e10e16fcacfdb9ca5d1a1b6724a2b6bf6a257a05bdd86ecfb8e8f2"
     end
 
     def install
@@ -20,8 +20,8 @@ class JpkiCli < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/siketyan/jpki-rs/releases/download/0.1.8/jpki-cli-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2b11ceebb5b19070b02f850b5665299de33fa20f8e145a597cd0d1e5e79b050d"
+      url "https://github.com/siketyan/jpki-rs/releases/download/0.2.0/jpki-cli-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d7d9982f87521d3062799073b064d6950dfb88f849237898c9afb6b6f8dd7e12"
 
       def install
         bin.install "jpki-cli"
