8cb68b4abbaec82d42337be01c7f2379c2ea8769
Natsuki Ikeguchi <me@s6n.jp>
2023-10-21 00:01:33 +0900

    feat(ghr): Use bottle

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -1,35 +1,21 @@
 class Ghr < Formula
   desc "Yet another repository management with auto-attaching profiles"
   homepage "https://github.com/siketyan/ghr"
+  url "https://github.com/siketyan/ghr/archive/refs/tags/v0.3.11.tar.gz"
+  sha256 "d5583898f1d881b63585afe226bb3a83acf389c874aa7ca348662efe4b71bf76"
   version "0.3.11"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/siketyan/ghr/releases/download/v0.3.11/ghr-x86_64-apple-darwin.tar.gz"
-      sha256 "29d3689f052a9cde9a21f99e1f73c45909712cf130f44d453a1490b9a50be7c9"
-    else
-      url "https://github.com/siketyan/ghr/releases/download/v0.3.11/ghr-aarch64-apple-darwin.tar.gz"
-      sha256 "bdf8c1b0695632eecd2b6ce9ebb3d4416163624c31fbe858b5134966207c4444"
-    end
-
-    def install
-      bin.install "ghr"
-    end
+  bottle do
   end
 
-  on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/siketyan/ghr/releases/download/v0.3.11/ghr-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "038825df6e7ac306ed5506481edf0f66433942e238e8bc5521feac2c76be88f3"
-    else
-      url "https://github.com/siketyan/ghr/releases/download/v0.3.11/ghr-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "677e6582d25bc3ba9b39a9459635dd24276f978936dbfdaab6a536dc445ecab9"
-    end
+  depends_on "rust" => :build
 
-    def install
-      bin.install "ghr"
-    end
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    bash_completion.install "resources/shell/bash/ghr-completion.bash"
+    fish_completion.install "resources/shell/fish/ghr-completion.fish"
   end
 
   test do
