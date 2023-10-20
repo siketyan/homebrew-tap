17b514df1253c2383105caa33aa0e1fbd07c4461
Natsuki Ikeguchi <me@s6n.jp>
2023-10-21 00:08:49 +0900

    style(ghr): Place version before sha256

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -2,8 +2,8 @@ class Ghr < Formula
   desc "Yet another repository management with auto-attaching profiles"
   homepage "https://github.com/siketyan/ghr"
   url "https://github.com/siketyan/ghr/archive/refs/tags/v0.3.11.tar.gz"
-  sha256 "d5583898f1d881b63585afe226bb3a83acf389c874aa7ca348662efe4b71bf76"
   version "0.3.11"
+  sha256 "d5583898f1d881b63585afe226bb3a83acf389c874aa7ca348662efe4b71bf76"
   license "MIT"
 
   depends_on "rust" => :build
